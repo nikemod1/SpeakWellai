dc032611528461620b606b9bb85fad6b3640b57d
nikemod1 <nike05data@gmail.com>
2025-07-31 22:10:44 +0530

    2Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white text-gray-800">
       <Navigation />
       <main className="flex-grow">
         {children}
