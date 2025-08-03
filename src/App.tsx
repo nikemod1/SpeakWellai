88b19ab292296ccfc97c10559b1b124d18baeeb5
speakwellai <speakwellai@gmail.com>
2025-08-03 14:53:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import Course from './pages/Course';
+import Course from './pages/Course'; 
+import Course from './pages/MarathiCourse';
 import JoinNow from './pages/JoinNow';
 import ChatbotPractice from './pages/ChatbotPractice';
 import Contact from './pages/Contact';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Course />} />
+          <Route path="/MarathiCourse" element={<MarathiCourse />} />
           <Route path="/join-now" element={<JoinNow />} />
           <Route path="/chatbot-practice" element={<ChatbotPractice />} />
           <Route path="/contact" element={<Contact />} />
