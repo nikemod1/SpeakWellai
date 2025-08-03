199b09b8e0062b73ca8147c60af8e2186389a43f
speakwellai <speakwellai@gmail.com>
2025-08-03 17:14:04 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -38,8 +38,8 @@ const Contact: React.FC = () => {
           <div className="mt-8 space-y-4">
             <p>
               <Mail className="inline-block w-5 h-5 mr-2 text-blue-600" />
-              <a href="mailto:support@speakwellai.com" className="text-blue-600 hover:underline">
-                support@speakwellai.com
+              <a href="mailto:speakwellai@gmail.com" className="text-blue-600 hover:underline">
+                speakwellai@gmail.com
               </a>
             </p>
             <p>
